e9ddcdf07cfddde0f41cf0f8aad59c50e86612f5
Ceagah <carlosceagah@gmail.com>
2025-03-15 16:52:49 -0300

    feat: change images

---
+++ src/app/_layout.tsx
@@ -0,0 +1,41 @@
+import { ThemeProvider } from "@/data/context/theme.context";
+import {
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_700Bold,
+  useFonts,
+} from "@expo-google-fonts/poppins";
+import {
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Slot } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
+
+
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_700Bold,
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#3B82F6" />
+      </View>
+    );
+  }
+
+  return (
+    <ThemeProvider>
+      <Slot />
+    </ThemeProvider>
+  );
+}
